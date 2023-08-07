fee1a159f282f582b689d664d81d84dbf01160fc
Solrac-616 <carlos.e1998g@gmail.com>
2023-08-07 08:19:25 -0400

    WHATSAPP

---
+++ src/components/WhatsApp.jsx
@@ -0,0 +1,12 @@
+
+const WhatsApp = () => {
+  return (
+    <div className="whatsapp-wrapper">
+        <div className="whatsapp-button">
+            
+        </div>
+    </div>
+  )
+}
+
+export default WhatsApp
--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import ThemeButtons from '../components/themeButtons'
+import WhatsApp from '../components/WhatsApp'
 
 const Root = () => {
   return (
     <>
       <Outlet />
       <ThemeButtons />
+      <WhatsApp />
     </>
   )
 }
