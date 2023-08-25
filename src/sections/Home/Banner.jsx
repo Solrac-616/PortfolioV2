3246c7816d811c932b8a746e6ca5e6af2236a060
Carlos Brito <carlos.e1998g@gmail.com>
2023-08-25 01:05:27 -0400

    pruebas de rueda 1

--- src/sections/Home/Banner.jsx
+++ src/sections/Home/Banner.jsx
@@ -1,3 +1,4 @@
+import Typed from 'react-typed';
 
 const Banner = () => {
   return (
@@ -5,7 +6,17 @@ const Banner = () => {
       <div className="row-default-v1 banner-content">
         <p>Hi!! this is my</p>
         <h2>PORTFOLIO</h2>
-        <p>I&apos;m Carlos Brito</p>
+        <div className="typed-banner">
+          <p>I&apos;m Carlos Brito</p>
+          <Typed
+              strings={["Full-Stack Developer", "How can we do?", "How can we do? React", "How can we do? React - MERN?", "How can we do? Angular", "How can we do? Angular + Node?", "How can we do? Next", "Alright", "Alright let's do it"]}
+              typeSpeed={100}
+              backSpeed={20}
+              backDelay={1000}
+              loop
+              smartBackspace
+            />
+        </div>
       </div>
       <div className="profession-container">
         <div className="profession-box font-raleway">
@@ -14,16 +25,16 @@ const Banner = () => {
             <h3>Full-Stack Developer</h3>
           </div>
           <div className="profession" style={{"--i": 1}}>
-            <i className="fa-brands fa-react"></i>
-            <h3>React</h3>
+            <i className="fa-brands fa-js"></i>
+            <h3>Next</h3>
           </div>
           <div className="profession" style={{"--i": 2}}>
             <i className="fa-brands fa-angular"></i>
             <h3>Angular</h3>
           </div>
           <div className="profession" style={{"--i": 3}}>
-            <i className="fa-brands fa-js"></i>
-            <h3>Next</h3>
+            <i className="fa-brands fa-react"></i>
+            <h3>React</h3>
           </div>
           <div className="circle">
 
