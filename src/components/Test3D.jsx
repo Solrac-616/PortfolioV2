46b45acdb09b74e6f17b21d8baa54f168e77e1f0
Carlos Brito <carlos.e1998g@gmail.com>
2023-08-27 18:58:59 -0400

    test 3d

--- src/components/Test3D.jsx
+++ src/components/Test3D.jsx
@@ -1,7 +1,24 @@
+import { OrbitControls } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
 
+Canvas
 const Test3D = () => {
   return (
-    <div>Test3D</div>
+    <section className="section-test">
+      <div>
+        <Canvas>
+          <OrbitControls enableZoom={false} autoRotate />
+          <ambientLight intensity={1} />
+          <directionalLight position={[3, 2, 1]}/>
+          <mesh>
+            {/* <shapeGeometry/> */}
+            <boxGeometry args={[2,2,2]}/>
+            <meshStandardMaterial color="#38bff8" />
+          </mesh>
+        </Canvas>
+      </div>
+    </section>
+    
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import "../Home.css"
 import Banner from "../sections/Home/Banner"
 import About from "../sections/Home/About"
 import Skills from "../sections/Home/Skills"
+import Test3D from "../components/Test3D"
 
 const Home = () => {
   return (
     <>
       <Banner />
       <About />
+      <Test3D />
       <Skills />
     </>
   )
