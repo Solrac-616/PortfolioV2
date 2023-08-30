2a56586a8ead3854ba28576125c9517a0d937569
Carlos Brito <carlos.e1998g@gmail.com>
2023-08-30 17:42:40 -0400

    colores socials

--- src/components/SocialsBtn.jsx
+++ src/components/SocialsBtn.jsx
@@ -25,18 +25,18 @@ const SocialsBtn = () => {
           </motion.div>
         </div>
         <div className="up">
-          <motion.a variants={fadeInDual('right', 'down', 'spring', 0.3 , 1)} href="https://wa.me/584123809750" target="_blank" className="socials-btn-card card1">
+          <motion.a data-cursor-color="#25d36569" variants={fadeInDual('right', 'down', 'spring', 0.3 , 1)} href="https://wa.me/584123809750" target="_blank" className="socials-btn-card card1">
             <i className="fa-brands fa-whatsapp whatsapp"></i>
           </motion.a>
-          <motion.a variants={fadeInDual('left', 'down', 'spring', 0.5 , 1)} href="https://www.linkedin.com/in/carlos-brito-carrero/" target="_blank" className="socials-btn-card card2">
+          <motion.a data-cursor-color="#0e76a869" variants={fadeInDual('left', 'down', 'spring', 0.5 , 1)} href="https://www.linkedin.com/in/carlos-brito-carrero/" target="_blank" className="socials-btn-card card2">
             <i className="fa-brands fa-linkedin linkedin"></i>
           </motion.a>
         </div>
         <div className="down">
-          <motion.a variants={fadeInDual('right', 'up', 'spring', 0.9 , 1)} href="https://github.com/Solrac-616" target="_blank" className="socials-btn-card card3">
+          <motion.a data-cursor-color="#F1502F69" variants={fadeInDual('right', 'up', 'spring', 0.9 , 1)} href="https://github.com/Solrac-616" target="_blank" className="socials-btn-card card3">
             <i className="fa-brands fa-github github"></i>
           </motion.a>
-          <motion.a variants={fadeInDual('left', 'up', 'spring', 0.7 , 1)} href="mailto:carlos.e1998g@gmail.com" target="_blank" className="socials-btn-card card4">
+          <motion.a data-cursor-color="#DB443769" variants={fadeInDual('left', 'up', 'spring', 0.7 , 1)} href="mailto:carlos.e1998g@gmail.com" target="_blank" className="socials-btn-card card4">
             <i className="fa-brands fa-google google"></i>
           </motion.a>
         </div>
