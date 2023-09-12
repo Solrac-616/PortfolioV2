a0f823ca99b5d737409f41de5a18000407cae13e
Carlos Brito <carlos.e1998g@gmail.com>
2023-09-11 23:20:40 -0400

    project cards v1

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,6 +1,9 @@
 import { motion } from "framer-motion"
 
 const ProjectItem = ({ project }) => {
+
+  const categorys = project.categorys
+
   return (
     <motion.div 
       layout
@@ -8,9 +11,18 @@ const ProjectItem = ({ project }) => {
       initial={{ opacity: 0 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
+      className="card-project"
     >
-      <h2>{project.name}</h2>
-      <img src={project.img} alt={project.name} />
+      <div className="card-img" style={{ backgroundImage: `url(${project.img})` }}>
+        {/* <img src={project.img} alt={project.name} /> */}
+      </div>
+      <div className="card-content">
+        <span className="name">{project.name}</span>
+        <h2>{project.name}</h2>
+        {categorys.map( item => (
+          <span key={item}>{item}</span>
+        ))}
+      </div>
     </motion.div>
   )
 }
