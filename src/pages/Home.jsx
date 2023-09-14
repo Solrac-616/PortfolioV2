09a15baaf045ff98956bd07d772889caf0a8d3f8
Solrac-616 <carlos.e1998g@gmail.com>
2023-09-13 21:12:12 -0400

    scroller home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,13 @@ import About from "../sections/Home/About"
 import Skills from "../sections/Home/Skills"
 import Projects from "../sections/Home/Projects"
 import Footer from "../sections/Footer"
+import MobileScroller from "../sections/Home/MobileScroller"
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <MobileScroller />
       <About />
       <Skills />
       <Projects />
---
+++ src/sections/Home/MobileScroller.jsx
@@ -0,0 +1,15 @@
+
+const MobileScroller = () => {
+  return (
+    <section id='mobileScroller' className='section-scroller'>
+        <div className="scroller-back">
+            <i className="fa-solid fa-chevron-down"></i>
+            <i className="fa-solid fa-chevron-down"></i>
+            <i className="fa-solid fa-chevron-down"></i>
+        </div>
+        <i className="fa-solid fa-hand-pointer"></i>
+    </section>
+  )
+}
+
+export default MobileScroller
