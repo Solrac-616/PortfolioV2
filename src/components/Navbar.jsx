d4aae943fb840dab0170139f678b37e90ed65b73
Carlos Brito <carlos.e1998g@gmail.com>
2023-09-14 12:56:03 -0400

    responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,12 @@ import { motion } from 'framer-motion';
 import { fadeIn, staggerContainer, zoomIn } from '../utils/motion';
 import useNavbar from '../Hooks/useNavbar';
 
+import '../customcss/burguer.css';
+
 const Navbar = () => {
   const { navState, renewNav } = useNavbar();
   const [active, setActive] = useState( navState ? navState : 'hero');
+  const [mobile, setMobile] = useState( false );
 
   // DETECCION DE SCROLL PARA EL NAVBAR
   const [scrolled, setScrolled] = useState(false);
@@ -64,8 +67,32 @@ const Navbar = () => {
             ))}
 
           </ul>
+          <div className={`mobile-burguer ${mobile ? 'active' : ''}`} onClick={() => setMobile(!mobile)} >
+            <span className="top"></span>
+            <span className="middle"></span>
+            <span className="bottom"></span>
+          </div>
         </div>
       </motion.nav>
+      <div className={`mobile-navbar ${mobile ? 'active' : ''}`} >
+        <div className="mobile-navbar-back" onClick={() => setMobile(false)}>
+
+        </div>
+        <ul className="mobile-links font-montserrat">
+          {navLinks.map((item) => (
+            <li key={item.title} >
+              <Customlink
+              id={item.id}
+              scrollId={item.scrollId}
+              name={item.name}
+              active={active}
+              onclick={()=>renewNav(item.name)}
+              title={item.title}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </header>
   )
 }
